29ae3cf4e0f1328e1d52f2cbd1c3f75fa557dc6e
Alessandro Zinni <alessandro.zinni98@icloud.com>
2023-03-02 16:21:06 +0100

    Changed number of epochs

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ gopro_dir = "./GOPRO_Large"
 saved_models_dir = "./saved_models"
 # Put here the directory of the repo if you are running in colab
 colab_dir = "/content/gdrive/My Drive/Stripformer/"
-epochs = 1
+epochs = 10
 batch_size = 8
 max_lr = 1e-4
 min_lr = 1e-7
-use_wandb = False
+use_wandb = True
