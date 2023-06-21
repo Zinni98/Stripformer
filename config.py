46356719447e99035ac8dd485c154308dcb91258
Alessandro Zinni <alessandro.zinni98@icloud.com>
2023-06-21 09:46:25 +0200

    Added test-only flag

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # flake8: noqa
 gopro_dir = "/media/dataset/GOPRO_Large"
-saved_models_dir = "/media/checkpoint/final.tar"  # Where to save the model during training. Set to None If don't want to save
+saved_models_dir = "/media/checkpoint/run6.tar"  # Where to save the model during training. Set to None If don't want to save
 colab_dir = "/content/gdrive/My Drive/Stripformer/"  # Put here the directory of the repo if you are running in colab
-load_dir = None  # Where to find already trained model
+load_dir = "/media/checkpoint/run4.tar"  # Where to find already trained model
 pretrain = False
 pre_train_epochs = 20
 epochs = 1000
@@ -12,4 +12,5 @@ max_lr = 1e-4
 min_lr = 1e-7
 pre_train_img_size = 256
 train_img_size = 256
-use_wandb = True  # Whether to log training status on Weights and biases, if unsure what this is, set to False
+test_only = False
+use_wandb = False  # Whether to log training status on Weights and biases, if unsure what this is, set to False
