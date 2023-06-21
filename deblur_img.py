cdf1c0d45938ad38de1f4f50cca5532c03002876
Alessandro Zinni <alessandro.zinni98@icloud.com>
2023-06-21 15:18:17 +0200

    minor commit

--- deblur_img.py
+++ deblur_img.py
@@ -7,7 +7,7 @@ from model.stripformer import Stripformer
 from PIL import Image
 
 
-def save_img(tensor_img, path="./example_images/deblurred/img1.png"):
+def save_img(tensor_img, path="./example_images/deblurred/img2.png"):
     if len(tensor_img.shape) == 4:
         tensor_img = torch.squeeze(tensor_img, 0)
     save_image(tensor_img, fp=path)
@@ -33,7 +33,7 @@ def main():
     network.load_state_dict(checkpoint["model_state_dict"])
     network.to("cpu")
     print("network loaded")
-    img = Image.open("./example_images/blurred/000008_blr.png").convert("RGB") # ./GOPRO_Large/test/GOPR0384_11_00/blur/000008.png
+    img = Image.open("./GOPRO_Large/test/GOPR0384_11_00/blur/000008.png").convert("RGB") # ./GOPRO_Large/test/GOPR0384_11_00/blur/000008.png
     db_img = deblur_img(network, img)
     save_img(db_img)
 
